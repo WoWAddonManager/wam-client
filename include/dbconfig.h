c3ebffd7c715863483866c17d5a16ae6a7d1a974
Jordan <jaayvazian@gmail.com>
2020-01-08 01:44:22 -0800

    Base db_config file

---
+++ include/dbconfig.h
@@ -0,0 +1,13 @@
+//
+// Created by jordan on 1/8/20.
+//
+
+#ifndef WAM_DBCONFIG_H
+#define WAM_DBCONFIG_H
+
+#include <string>
+
+const std::string username = "";
+const std::string passowrd = "";
+
+#endif //WAM_DBCONFIG_H
