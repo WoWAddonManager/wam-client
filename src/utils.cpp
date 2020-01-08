1b6d37afc8753fd203a0cbcd01531085827affed
Jordan <jaayvazian@gmail.com>
2020-01-08 01:18:25 -0800

    Added read_file_to_string() and string_to_json()
    read_file_to_string() take in a std::string as a parameter that is a path to a file and attempts to read that file into a std::string  
    
    string_to_json() takes in a string as a parameter parses a string into a Json::Value

---
+++ src/utils.cpp
@@ -0,0 +1,28 @@
+#include "utils.h"
+#include <fstream>
+#include <sstream>
+#include <json/json.h>
+#include <iostream>
+std::string utils::read_file_to_string(const std::string &path) {
+    std::ifstream file = std::ifstream(path, std::ios::in);
+    std::stringstream ss;
+    ss << file.rdbuf();
+    std::string content = ss.str();
+    return content;
+}
+
+Json::Value utils::string_to_json(const std::string &string){
+    Json::Value json;
+    Json::CharReaderBuilder builder;
+    Json::CharReader *reader = builder.newCharReader();
+    std::string errors;
+    bool parsed = reader->parse(string.c_str(), string.c_str() + string.size(), &json,
+                                &errors);
+    if (!parsed) {
+        std::cout << errors << std::endl;
+        return json["error"];
+    }
+    else {
+        return json;
+    }
+}
