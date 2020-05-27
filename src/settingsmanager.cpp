2c14c2145128b0395578d3387ffc647166d6e7c0
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 16:50:19 -0700

    Move settings file population to separate function for readability.

--- src/settingsmanager.cpp
+++ src/settingsmanager.cpp
@@ -4,26 +4,32 @@
 #include <json/json.h>
 #include <boost/filesystem.hpp>
 #include <string>
-#include <iostream>
-#include <fstream>
-#include <ostream>
 #include "settingsmanager.h"
 #include "utils.h"
 #include "mainwindow.h"
 
 
 
+
 void SettingsManager::set_wow_folder_paths() {
-    auto file = std::fstream("settings.json");
     this->settings_root["wow_path"] = this->get_base_wow_path();
     this->m_retail_addons_path = this->m_base_wow_folder_path + "/_retail_/Interface/AddOns";
     this->m_classic_addons_path = this->m_base_wow_folder_path + "/_classic_/Interface/AddOns";
     this->m_ptr_addons_path = this->m_base_wow_folder_path + "/_ptr_/Interface/AddOns";
-    file << this->settings_root;
-    file.close();
+    auto logger = this->m_logger;
+    if(logger) {
+        logger->info("Retail Addon Path: {}", this->m_retail_addons_path);
+        logger->info("Classic Addon Path: {}", this->m_classic_addons_path);
+        logger->info("PTR Addon Path: {}", this->m_ptr_addons_path);
+    }
+
 }
 
 SettingsManager::SettingsManager() {
+    this->m_logger = spdlog::get("SettingsManager");
+    if(!this->m_logger){
+       this->m_logger = initialize_logger("SettingsManager");
+    }
     generate_settings_file();
     generate_folder_structure();
 }
@@ -36,25 +42,25 @@ std::string SettingsManager::get_base_wow_path() const {
     return this->m_base_wow_folder_path;
 }
 
-void SettingsManager::set_base_wow_path(const std::string &path) {
-    this->m_base_wow_folder_path = path;
+void SettingsManager::set_base_wow_path(const QString &path) {
+    this->m_base_wow_folder_path.assign(path.toStdString());
     set_wow_folder_paths();
 }
 
 void SettingsManager::generate_settings_file() {
-    if (boost::filesystem::exists("settings.json") && boost::filesystem::is_empty("settings.json")) {
-        this->settings_root["wow_path"] = "";
-        this->settings_root["token"] = "";
-        this->settings_root["client"] = "";
-        this->settings_root["uid"] = "";
-        this->settings_root["first_time"] = true;
-        auto file = std::ofstream("settings.json");
-        file << this->settings_root;
+    if (boost::filesystem::exists("settings.json")) {
+        if (boost::filesystem::is_empty("settings.json")) {
+            this->populate_settings_file();
+        }
+        else {
+            update_settings_from_file();
+        }
     }
     else {
-        this->update_settings_from_file();
+        auto file = std::ofstream("settings.json");
+        file.close();
+        this->populate_settings_file();
     }
-
 }
 
 void SettingsManager::generate_folder_structure() {
@@ -75,6 +81,7 @@ void SettingsManager::update_client(const std::string &client) {
 }
 
 void SettingsManager::update_settings_file(const std::string &field, const std::string &value) {
+
     this->settings_root = string_to_json(read_file_to_string("settings.json"));
     this->settings_root[field] = value;
     std::ofstream file("settings.json");
@@ -108,3 +115,22 @@ void SettingsManager::update_settings_from_file() {
     this->m_client = this->settings_root["client"].asString();
     this->m_uid = this->settings_root["uid"].asString();
 }
+
+void SettingsManager::populate_settings_file() {
+    auto file = std::ofstream("settings.json");
+    this->settings_root["wow_path"] = "";
+    this->settings_root["token"] = "";
+    this->settings_root["token"].setComment(std::string("//!!!DO NOT SHARE THIS!!!"), Json::commentBefore);
+    this->settings_root["client"] = "";
+    this->settings_root["client"].setComment(std::string("//!!!DO NOT SHARE THIS!!!"), Json::commentBefore);
+    this->settings_root["uid"] = "";
+    this->settings_root["uid"].setComment(std::string("//!!!DO NOT SHARE THIS!!!"), Json::commentBefore);
+    this->settings_root["first_time"] = true;
+    file << this->settings_root;
+    file.close();
+}
+
+const std::shared_ptr<spdlog::logger> &SettingsManager::get_logger() const {
+    return m_logger;
+}
+
