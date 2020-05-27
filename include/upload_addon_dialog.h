53a0d8441b9fb31dad8a78b90a297946d7b73825
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 16:53:48 -0700

    Code cleanup/formatting.

--- include/upload_addon_dialog.h
+++ include/upload_addon_dialog.h
@@ -1,17 +1,20 @@
 #ifndef WAM_UPLOAD_ADDON_DIALOG_H
 #define WAM_UPLOAD_ADDON_DIALOG_H
+
 #include "ui_upload_addon_dialog.h"
 #include "addon.h"
+
 namespace Ui {
     class upload_addon_dialog;
 }
 
-class upload_addon_dialog : public QDialog{
-    Q_OBJECT
+class upload_addon_dialog : public QDialog {
+Q_OBJECT
 
 public:
     explicit upload_addon_dialog(QWidget *parent = nullptr);
-    ~upload_addon_dialog() override ;
+
+    ~upload_addon_dialog() override;
 
 private:
     Ui::upload_addon_dialog *dialog;
--- src/upload_addon_dialog.cpp
+++ src/upload_addon_dialog.cpp
@@ -2,7 +2,7 @@
 #include "addon.h"
 #include "upload_addon_dialog.h"
 
-upload_addon_dialog::upload_addon_dialog(QWidget *parent): QDialog(parent), dialog(new Ui::upload_addon_dialog){
+upload_addon_dialog::upload_addon_dialog(QWidget *parent) : QDialog(parent), dialog(new Ui::upload_addon_dialog) {
     dialog->setupUi(this);
     dialog->wow_version->addItem("Retail");
     dialog->wow_version->addItem("Classic");
@@ -14,21 +14,21 @@ upload_addon_dialog::upload_addon_dialog(QWidget *parent): QDialog(parent), dial
         std::cout << folder_path.toStdString() << std::endl;
     });
 
-    connect(dialog->upload_addon, &QPushButton::clicked, [&](){
+    connect(dialog->upload_addon, &QPushButton::clicked, [&]() {
         auto path = dialog->addon_path->text().toStdString();
         auto addon_name = dialog->addon_name->text().toStdString();
         auto addon_version = dialog->addon_version->text().toStdString();
         auto addon_description = dialog->addon_description_textbox->document()->toPlainText().toStdString();
         auto wow_version = dialog->wow_version->itemData(dialog->wow_version->currentIndex()).toString().toStdString();
-       if(!path.empty() && !addon_name.empty() && !addon_version.empty() && !addon_description.empty()){
-           Addon addon{};
-           addon.m_addonName = addon_name;
-           addon.m_addon_version = addon_version;;
-           addon.m_wow_version = Addon::wow_version_to_enum(wow_version);
-           addon.m_description = addon_description;
-           addon.make_wam(path);
-           std::cout << addon;
-       }
+        if (!path.empty() && !addon_name.empty() && !addon_version.empty() && !addon_description.empty()) {
+            Addon addon{};
+            addon.m_addonName = addon_name;
+            addon.m_addon_version = addon_version;;
+            addon.m_wow_version = Addon::wow_version_to_enum(wow_version);
+            addon.m_description = addon_description;
+            addon.make_wam(path);
+            std::cout << addon;
+        }
     });
 }
 
