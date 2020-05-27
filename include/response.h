782e6250783ad3ca04d67b4824e0cd939e3edb7f
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 16:52:51 -0700

    Clean up some syntax and mark Response constructor as explicit.

--- include/response.h
+++ include/response.h
@@ -8,45 +8,33 @@
 #include <string>
 #include <boost/optional.hpp>
 #include <iostream>
-template <class T>
+
+template<class T>
 class Response {
 public:
     ~Response() = default;
-    Response() = delete;
 
-    Response(const std::string &p_message, const int &p_error_code, const boost::optional<T> &p_data) {
+    explicit Response(const std::string &p_message, const boost::optional<T> &p_data) {
         this->m_message = p_message;
-        this->m_error_code = p_error_code;
         this->m_data = p_data;
-
     }
 
-    boost::optional<T> get_data(){
-        if(this->m_data != boost::none)
-            return boost::optional<T>{this->m_data};
-        else
-            return boost::none;
-
+    boost::optional<T> get_data() {
+        return this->m_data;
     }
 
     std::string get_message() {
         return this->m_message;
     }
-    int get_error_code() {
-        return this->m_error_code;
-    }
 
-    friend std::ostream &operator<<(std::ostream &os, const Response<T> response){
+    friend std::ostream &operator<<(std::ostream &os, const Response<T> &response) {
         os << std::string("Message: ") << response.m_message << std::endl;
-        os << std::string("Status: ") << response.m_error_code << std::endl;
         return os;
     }
 
 
-
 private:
-    std::string m_message = "";
-    int m_error_code = 0;
+    std::string m_message;
     boost::optional<T> m_data;
 
 
