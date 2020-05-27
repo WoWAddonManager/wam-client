ddd91eaa3e3d3ff7912e66acca172c9c6adc1652
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 16:46:16 -0700

    Implement wrapper function for creating the logger with some good defaults

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,9 @@
 #ifndef WAM_UTILS_H
 #define WAM_UTILS_H
+
 #include <string>
 #include <json/json.h>
+#include <spdlog/spdlog.h>
 #include "httplib.h"
 
 const static char BASE_AUTH_ROUTE[] = "/auth";
@@ -10,9 +12,14 @@ const static char LOGOUT_AUTH_ROUTE[] = "/auth/sign_out";
 const static char VALIDATE_TOKEN_ROUTE[] = "/auth/validate_token";
 
 
-std::string read_file_to_string(const std::string &path);
+extern std::string read_file_to_string(const std::string &path);
+
 Json::Value string_to_json(const std::basic_string<char> &string);
+
 httplib::Client make_client();
+
 void make_message_box(const std::string &message);
+
+std::shared_ptr<spdlog::logger> initialize_logger(const std::string &name = "logger");
 
 #endif //WAM_UTILS_H
