713e87efdb1d7f053b12823dc067b4d1bc60b1ef
Jordan Ayvazian <jaayvazian@gmail.com>
2020-05-27 16:53:23 -0700

    Set up some general new ui stuff for uploading addons.

--- include/ui_upload_addon_dialog.h
+++ include/ui_upload_addon_dialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'upload_addon_dialog.ui'
 **
-** Created by: Qt User Interface Compiler version 5.14.1
+** Created by: Qt User Interface Compiler version 5.14.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -21,27 +21,25 @@
 
 QT_BEGIN_NAMESPACE
 
-class Ui_upload_addon_dialog
-{
+class Ui_upload_addon_dialog {
 public:
     QGridLayout *gridLayout;
-    QComboBox *wow_version;
-    QTextEdit *addon_description_textbox;
-    QLabel *label_4;
-    QLineEdit *addon_name;
-    QLineEdit *lineEdit;
-    QLineEdit *addon_path;
     QLabel *label_5;
     QLabel *label_2;
+    QLineEdit *addon_name;
+    QLineEdit *lineEdit;
     QLabel *label_3;
-    QLabel *label;
     QPushButton *browse_addon_folder_button;
-    QLabel *label_6;
+    QComboBox *wow_version;
+    QLabel *label_4;
     QLineEdit *addon_version;
     QPushButton *upload_addon;
+    QLabel *label;
+    QLabel *label_6;
+    QLineEdit *addon_path;
+    QTextEdit *addon_description_textbox;
 
-    void setupUi(QDialog *upload_addon_dialog)
-    {
+    void setupUi(QDialog *upload_addon_dialog) {
         if (upload_addon_dialog->objectName().isEmpty())
             upload_addon_dialog->setObjectName(QString::fromUtf8("upload_addon_dialog"));
         upload_addon_dialog->setWindowModality(Qt::NonModal);
@@ -56,88 +54,51 @@ public:
         upload_addon_dialog->setModal(false);
         gridLayout = new QGridLayout(upload_addon_dialog);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        wow_version = new QComboBox(upload_addon_dialog);
-        wow_version->setObjectName(QString::fromUtf8("wow_version"));
-        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Fixed);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(wow_version->sizePolicy().hasHeightForWidth());
-        wow_version->setSizePolicy(sizePolicy1);
-        QFont font;
-        font.setPointSize(9);
-        wow_version->setFont(font);
-
-        gridLayout->addWidget(wow_version, 1, 7, 1, 2);
-
-        addon_description_textbox = new QTextEdit(upload_addon_dialog);
-        addon_description_textbox->setObjectName(QString::fromUtf8("addon_description_textbox"));
-        addon_description_textbox->setFont(font);
-
-        gridLayout->addWidget(addon_description_textbox, 3, 0, 1, 9);
-
-        label_4 = new QLabel(upload_addon_dialog);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
-        QSizePolicy sizePolicy2(QSizePolicy::Fixed, QSizePolicy::Preferred);
-        sizePolicy2.setHorizontalStretch(0);
-        sizePolicy2.setVerticalStretch(0);
-        sizePolicy2.setHeightForWidth(label_4->sizePolicy().hasHeightForWidth());
-        label_4->setSizePolicy(sizePolicy2);
-        label_4->setFont(font);
-
-        gridLayout->addWidget(label_4, 1, 6, 1, 1);
-
-        addon_name = new QLineEdit(upload_addon_dialog);
-        addon_name->setObjectName(QString::fromUtf8("addon_name"));
-        QSizePolicy sizePolicy3(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        sizePolicy3.setHorizontalStretch(0);
-        sizePolicy3.setVerticalStretch(0);
-        sizePolicy3.setHeightForWidth(addon_name->sizePolicy().hasHeightForWidth());
-        addon_name->setSizePolicy(sizePolicy3);
-        addon_name->setFont(font);
-
-        gridLayout->addWidget(addon_name, 1, 1, 1, 1);
-
-        lineEdit = new QLineEdit(upload_addon_dialog);
-        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
-        sizePolicy3.setHeightForWidth(lineEdit->sizePolicy().hasHeightForWidth());
-        lineEdit->setSizePolicy(sizePolicy3);
-        lineEdit->setFont(font);
-
-        gridLayout->addWidget(lineEdit, 1, 3, 1, 1);
-
-        addon_path = new QLineEdit(upload_addon_dialog);
-        addon_path->setObjectName(QString::fromUtf8("addon_path"));
-        addon_path->setFont(font);
-
-        gridLayout->addWidget(addon_path, 0, 1, 1, 7);
-
         label_5 = new QLabel(upload_addon_dialog);
         label_5->setObjectName(QString::fromUtf8("label_5"));
+        QFont font;
+        font.setPointSize(9);
         label_5->setFont(font);
 
         gridLayout->addWidget(label_5, 2, 0, 1, 1);
 
         label_2 = new QLabel(upload_addon_dialog);
         label_2->setObjectName(QString::fromUtf8("label_2"));
-        sizePolicy2.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
-        label_2->setSizePolicy(sizePolicy2);
+        QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
+        label_2->setSizePolicy(sizePolicy1);
         label_2->setFont(font);
 
         gridLayout->addWidget(label_2, 1, 0, 1, 1);
 
+        addon_name = new QLineEdit(upload_addon_dialog);
+        addon_name->setObjectName(QString::fromUtf8("addon_name"));
+        QSizePolicy sizePolicy2(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(addon_name->sizePolicy().hasHeightForWidth());
+        addon_name->setSizePolicy(sizePolicy2);
+        addon_name->setFont(font);
+
+        gridLayout->addWidget(addon_name, 1, 1, 1, 1);
+
+        lineEdit = new QLineEdit(upload_addon_dialog);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        sizePolicy2.setHeightForWidth(lineEdit->sizePolicy().hasHeightForWidth());
+        lineEdit->setSizePolicy(sizePolicy2);
+        lineEdit->setFont(font);
+
+        gridLayout->addWidget(lineEdit, 1, 3, 1, 1);
+
         label_3 = new QLabel(upload_addon_dialog);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        sizePolicy2.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
-        label_3->setSizePolicy(sizePolicy2);
+        sizePolicy1.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
+        label_3->setSizePolicy(sizePolicy1);
         label_3->setFont(font);
 
         gridLayout->addWidget(label_3, 1, 4, 1, 1);
-
-        label = new QLabel(upload_addon_dialog);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setFont(font);
-
-        gridLayout->addWidget(label, 0, 0, 1, 1);
 
         browse_addon_folder_button = new QPushButton(upload_addon_dialog);
         browse_addon_folder_button->setObjectName(QString::fromUtf8("browse_addon_folder_button"));
@@ -145,16 +106,32 @@ public:
 
         gridLayout->addWidget(browse_addon_folder_button, 0, 8, 1, 1);
 
-        label_6 = new QLabel(upload_addon_dialog);
-        label_6->setObjectName(QString::fromUtf8("label_6"));
-        label_6->setFont(font);
+        wow_version = new QComboBox(upload_addon_dialog);
+        wow_version->addItem(QString());
+        wow_version->addItem(QString());
+        wow_version->addItem(QString());
+        wow_version->setObjectName(QString::fromUtf8("wow_version"));
+        QSizePolicy sizePolicy3(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(wow_version->sizePolicy().hasHeightForWidth());
+        wow_version->setSizePolicy(sizePolicy3);
+        wow_version->setFont(font);
 
-        gridLayout->addWidget(label_6, 1, 2, 1, 1);
+        gridLayout->addWidget(wow_version, 1, 7, 1, 2);
+
+        label_4 = new QLabel(upload_addon_dialog);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        sizePolicy1.setHeightForWidth(label_4->sizePolicy().hasHeightForWidth());
+        label_4->setSizePolicy(sizePolicy1);
+        label_4->setFont(font);
+
+        gridLayout->addWidget(label_4, 1, 6, 1, 1);
 
         addon_version = new QLineEdit(upload_addon_dialog);
         addon_version->setObjectName(QString::fromUtf8("addon_version"));
-        sizePolicy3.setHeightForWidth(addon_version->sizePolicy().hasHeightForWidth());
-        addon_version->setSizePolicy(sizePolicy3);
+        sizePolicy2.setHeightForWidth(addon_version->sizePolicy().hasHeightForWidth());
+        addon_version->setSizePolicy(sizePolicy2);
         addon_version->setFont(font);
 
         gridLayout->addWidget(addon_version, 1, 5, 1, 1);
@@ -164,30 +141,65 @@ public:
 
         gridLayout->addWidget(upload_addon, 4, 8, 1, 1);
 
+        label = new QLabel(upload_addon_dialog);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setFont(font);
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        label_6 = new QLabel(upload_addon_dialog);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setFont(font);
+
+        gridLayout->addWidget(label_6, 1, 2, 1, 1);
+
+        addon_path = new QLineEdit(upload_addon_dialog);
+        addon_path->setObjectName(QString::fromUtf8("addon_path"));
+        addon_path->setFont(font);
+
+        gridLayout->addWidget(addon_path, 0, 1, 1, 7);
+
+        addon_description_textbox = new QTextEdit(upload_addon_dialog);
+        addon_description_textbox->setObjectName(QString::fromUtf8("addon_description_textbox"));
+        addon_description_textbox->setFont(font);
+
+        gridLayout->addWidget(addon_description_textbox, 3, 0, 1, 9);
+
+        QWidget::setTabOrder(addon_path, browse_addon_folder_button);
+        QWidget::setTabOrder(browse_addon_folder_button, addon_name);
+        QWidget::setTabOrder(addon_name, lineEdit);
+        QWidget::setTabOrder(lineEdit, addon_version);
+        QWidget::setTabOrder(addon_version, wow_version);
+        QWidget::setTabOrder(wow_version, addon_description_textbox);
+        QWidget::setTabOrder(addon_description_textbox, upload_addon);
 
         retranslateUi(upload_addon_dialog);
 
         QMetaObject::connectSlotsByName(upload_addon_dialog);
     } // setupUi
 
-    void retranslateUi(QDialog *upload_addon_dialog)
-    {
-        upload_addon_dialog->setWindowTitle(QCoreApplication::translate("upload_addon_dialog", "Upload Addon", nullptr));
-        label_4->setText(QCoreApplication::translate("upload_addon_dialog", "WoW Version", nullptr));
+    void retranslateUi(QDialog *upload_addon_dialog) {
+        upload_addon_dialog->setWindowTitle(
+            QCoreApplication::translate("upload_addon_dialog", "Upload Addon", nullptr));
         label_5->setText(QCoreApplication::translate("upload_addon_dialog", "Description", nullptr));
         label_2->setText(QCoreApplication::translate("upload_addon_dialog", "Addon Name", nullptr));
         label_3->setText(QCoreApplication::translate("upload_addon_dialog", "WoW Patch", nullptr));
-        label->setText(QCoreApplication::translate("upload_addon_dialog", "Addon Path", nullptr));
         browse_addon_folder_button->setText(QCoreApplication::translate("upload_addon_dialog", "Browse", nullptr));
-        label_6->setText(QCoreApplication::translate("upload_addon_dialog", "Version", nullptr));
+        wow_version->setItemText(0, QCoreApplication::translate("upload_addon_dialog", "Retail", nullptr));
+        wow_version->setItemText(1, QCoreApplication::translate("upload_addon_dialog", "Classic", nullptr));
+        wow_version->setItemText(2, QCoreApplication::translate("upload_addon_dialog", "PTR", nullptr));
+
+        label_4->setText(QCoreApplication::translate("upload_addon_dialog", "WoW Version", nullptr));
         addon_version->setText(QString());
         upload_addon->setText(QCoreApplication::translate("upload_addon_dialog", "Upload", nullptr));
+        label->setText(QCoreApplication::translate("upload_addon_dialog", "Addon Path", nullptr));
+        label_6->setText(QCoreApplication::translate("upload_addon_dialog", "Version", nullptr));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class upload_addon_dialog: public Ui_upload_addon_dialog {};
+    class upload_addon_dialog : public Ui_upload_addon_dialog {};
 } // namespace Ui
 
 QT_END_NAMESPACE
